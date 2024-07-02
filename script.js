f39c6ad73985e18746013b1920820fb712c8cff4
Emil De Guzman <em.deguzm@gmail.com>
2024-07-02 10:18:25 -0500

    update website page

--- script.js
+++
@@ -1,8 +0,0 @@
-  // Get the current year
-  const currentYear = new Date().getFullYear();
-
-  // Find the element by its id
-  const yearElement = document.getElementById('current-year');
-
-  // Update the element's content with the current year
-  yearElement.textContent = currentYear;
